30aafe447841ee68ee08601bd30615671948e2b9
Renddi Yeh <renddi@relab.cc>
2020-06-19 16:21:10 +0800

    Updates

---
+++ precache-manifest.91958e76b74eed325e9ae9929ea8fcba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01e6ec77830ffddbb048dd4fc24c9d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53418ab715c89f0687cf",
+    "url": "/static/css/2.68c6a531.chunk.css"
+  },
+  {
+    "revision": "53418ab715c89f0687cf",
+    "url": "/static/js/2.0ecfa3bf.chunk.js"
+  },
+  {
+    "revision": "2f5721c1452e82ee2210f85473256d7f",
+    "url": "/static/js/2.0ecfa3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06f4c25c083b39fa123",
+    "url": "/static/js/main.63f060f3.chunk.js"
+  },
+  {
+    "revision": "6055c7b380776741c4e0",
+    "url": "/static/js/runtime-main.d38cf63e.js"
+  }
+]);
--- styles-894d04203805a5d8b74f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{184:function(n,o,w){},321:function(n,o,w){}}]);
-//# sourceMappingURL=styles-894d04203805a5d8b74f.js.map
